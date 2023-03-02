78afb3251e091d6a228ec1bd70c49178419adc66
Matt Bengston <bengsfort@gmail.com>
2023-03-02 22:02:39 +0200

    Adjust page layout slightly and make top padding configurable per page.

--- packages/app/src/layout/Page/Page.tsx
+++ packages/app/src/layout/Page/Page.tsx
@@ -1,17 +1,19 @@
 import type {ComponentChildren} from 'preact';
+import classNames               from 'classnames';
 
 import {PageNav}                from '../PageNav';
 
 import styles                   from './Page.module.css';
 
 interface Props {
+  hasNoPadding?: boolean;
   children: ComponentChildren;
 }
-export function Page({children}: Props) {
+export function Page({children, hasNoPadding = false}: Props) {
   return (
     <>
       <PageNav />
-      <main class={styles.contentAreaWrapper}>
+      <main class={classNames(styles.contentAreaWrapper, {[styles.noPadding]: hasNoPadding})}>
         {children}
       </main>
     </>
