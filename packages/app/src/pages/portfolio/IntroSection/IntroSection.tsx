d929d08bdbebc3cbb696100d853d5ddcc2cf44e8
bengsfort <bengsfort@gmail.com>
2023-03-09 15:08:12 +0200

    Add WavyText component. Use it for scroll indicator

--- packages/app/src/pages/portfolio/IntroSection/IntroSection.tsx
+++ packages/app/src/pages/portfolio/IntroSection/IntroSection.tsx
@@ -1,6 +1,7 @@
 import styles         from './IntroSection.module.css';
 import {FlyoutLabels} from './FlyoutLabels';
 import {StarField}    from './StarField';
+import {WavyText}     from './WavyText';
 
 export function IntroSection() {
   const specialtyList = [
@@ -15,10 +16,11 @@ export function IntroSection() {
     <section class={styles.sectionWrapper}>
       <StarField />
       <div class={styles.textWrapper}>
-        <h1 className={styles.heroHeader}>Hey, my name is <strong>Matt</strong>.</h1>
-        <p className={styles.leadText}>I'm a software developer who specialises in <FlyoutLabels labels={specialtyList}>many things</FlyoutLabels> currently based in Helsinki, Finland.</p>
-        <p className={styles.leadText}>I focus mainly on UI, developer tooling, and game development.</p>
+        <h1 class={styles.heroHeader}>Hey, my name is <strong>Matt</strong>.</h1>
+        <p class={styles.leadText}>I'm a software developer who specialises in <FlyoutLabels labels={specialtyList}>many things</FlyoutLabels> currently based in Helsinki, Finland.</p>
+        <p class={styles.leadText}>I focus mainly on UI, developer tooling, and game development.</p>
       </div>
+      <WavyText className={styles.scrollIndicator} text={`scroll`} />
     </section>
   );
 }
