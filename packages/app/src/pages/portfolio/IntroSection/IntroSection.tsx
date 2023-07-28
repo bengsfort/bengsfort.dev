a413d0fe0d63baa1c8cfd7e6945d8f1eb0883c66
Matt Bengston <bengsfort@gmail.com>
2023-07-28 15:32:48 +0300

    Tweak media queries slightly.

--- packages/app/src/pages/portfolio/IntroSection/IntroSection.tsx
+++ packages/app/src/pages/portfolio/IntroSection/IntroSection.tsx
@@ -31,8 +31,9 @@ export function IntroSection() {
       <StarField className={styles.starLayer} noAnimation={reduceMotion} />
       <div class={classNames(styles.textWrapper, visible)}>
         <h1 class={styles.heroHeader}>Hey, my name is <strong>Matt</strong>.</h1>
-        <p class={styles.leadText}>I'm a software developer who specialises in <FlyoutLabels labels={specialtyList}>many things</FlyoutLabels> currently based in Helsinki, Finland.</p>
-        <p class={styles.leadText}>I focus mainly on UI, developer tooling, and game development.</p>
+        <span class={styles.heroHeader}>I'm a software developer passionate about delivering high quality experiences that feel good.</span>
+        <p class={styles.leadText}>I specialise in UI for web, games, and mobile as well as developer tooling and game programming.</p>
+        <p class={styles.leadText}>I am currently based in Helsinki, Finland and building cool stuff with <a href="https://noice.com" target="_blank">Noice</a> (previously <a href="https://epicgames.com" target="_blank">Epic Games</a>, <a href="https://unity3d.com" target="_blank">Unity Technologies</a>).</p>
       </div>
       <WavyText className={classNames(styles.scrollIndicator, visible)} text={`scroll`} />
     </section>
