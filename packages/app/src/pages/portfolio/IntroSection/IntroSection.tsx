a8f59978dc3e59d58693c892e892801f684f9aca
Matt Bengston <bengsfort@gmail.com>
2023-08-01 22:42:53 +0300

    Clean up intro section copy, increase spacing for less clunkiness.

--- packages/app/src/pages/portfolio/IntroSection/IntroSection.tsx
+++ packages/app/src/pages/portfolio/IntroSection/IntroSection.tsx
@@ -29,9 +29,10 @@ export function IntroSection() {
       <StarField noAnimation={reduceMotion || !inViewport} />
       <div class={classNames(styles.textWrapper, visible)}>
         <div class={styles.hero}>
-          <h1>Hey, my name is <strong>Matt</strong>.</h1> I'm a software developer passionate about high quality experiences that <em class={styles.emphasizedText}>feel good</em>. I specialise in
+          <h1>Hey, my name is <strong>Matt</strong>;</h1>
+          and I specialise in
         </div>
-        <TextCarousel items={specialtyList} interval={2000} pause={!inViewport} />
+        <TextCarousel items={specialtyList} pause={!inViewport} />
         <p class={styles.leadText}>I am currently based in Helsinki, Finland and building cool stuff with <a href={`https://noice.com`} target={`_blank`}>Noice</a> (previously <a href={`https://epicgames.com`} target={`_blank`}>Epic Games</a>, <a href={`https://unity3d.com`} target={`_blank`}>Unity Technologies</a>).</p>
       </div>
     </section>
