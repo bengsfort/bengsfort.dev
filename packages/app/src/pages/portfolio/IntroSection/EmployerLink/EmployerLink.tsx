e5c75ce9f528177ca5817563fb2ed776c1535e1d
Matt Bengston <bengsfort@gmail.com>
2023-08-09 12:00:39 +0300

    Update unity icon, add note about reasoning for secondary span

--- packages/app/src/pages/portfolio/IntroSection/EmployerLink/EmployerLink.tsx
+++ packages/app/src/pages/portfolio/IntroSection/EmployerLink/EmployerLink.tsx
@@ -1,5 +1,13 @@
 import styles from './EmployerLink.module.css';
 
+/**
+ * Note: We use a <span> to duplicate the text for the CSS animation we
+ * could 100% use either text-shadow or a psuedo element for this, but
+ * - Animating text-shadow is not a good idea for performance
+ * - Animating a psuedo element is not a good idea for accessibility
+ * - span allows us to have performance AND hide the text from screen readers
+ */
+
 interface Props {
   label: string;
   href: string;
@@ -7,7 +15,7 @@ interface Props {
 }
 export function EmployerLink({label, href, iconUrl}: Props) {
   return (
-    <a href={href} target={`_blank`} rel={`noopener noreferrer`} class={styles.link} data-label={label}>
+    <a href={href} target={`_blank`} rel={`noopener noreferrer external`} class={styles.link} data-label={label}>
       <img aria-hidden={`true`} src={iconUrl} alt={`${label} logo`} class={styles.icon} />
       {label}
       <span aria-hidden={`true`} class={styles.linkTop}>{label}</span>
