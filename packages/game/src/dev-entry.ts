cdb5cf36ad5872a793d8b042ca8eb4d2220e5fb7
Matt Bengston <bengsfort@gmail.com>
2024-05-09 13:33:49 +0300

    Scaffold API

--- packages/game/src/dev-entry.ts
+++ packages/game/src/dev-entry.ts
@@ -1,3 +1,14 @@
-import { initGame } from './game';
+import { initGame, runGame } from './game';
+import './dev-entry.module.css';
 
-initGame();
+async function main() {
+  try {
+    const el = await initGame();
+    document.body.appendChild(el);
+    runGame();
+  } catch (err) {
+    console.error(err); // eslint-disable-line no-console
+  }
+}
+
+main();
--- packages/game/src/game.ts
+++ packages/game/src/game.ts
@@ -1,3 +1,29 @@
-export const initGame = () => {
-  console.log('Henlooo');
+// Main game entry point and API definitions.
+// This module should be the main place where the game is controlled.
+
+// @todo (Matti) - Expose events? Game state change, jank detected, etc?
+export type GameMode = 'idle' | 'game';
+
+export const initGame = (): Promise<HTMLCanvasElement> => {
+  // Create window, globals, consts, etc.
+  // Load assets
+  return Promise.reject();
+};
+
+export const runGame = () => {
+  // Start game loop
+};
+
+export const setGameMode = (_: GameMode) => {
+  // Set game mode
+  // Update game scenes
+};
+
+export const pauseGame = () => {
+  // Pause game loop
+};
+
+export const stopGame = () => {
+  // Stop game loop
+  // Clean up
 };
