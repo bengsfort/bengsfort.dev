fd13617b1bf5346376bd4f6600bf2f7be61144b0
bengsfort <bengsfort@gmail.com>
2025-02-09 15:15:08 +0200

    Flesh out placeholder scene a bit more

--- packages/bg-game/src/main.ts
+++ packages/bg-game/src/main.ts
@@ -3,16 +3,12 @@ import {
   Mesh,
   MeshBasicMaterial,
   PerspectiveCamera,
+  PlaneGeometry,
   Scene,
   Vector3,
   WebGLRenderer,
 } from "three";
-
-function makeCube(): Mesh {
-  const geo = new BoxGeometry(1, 1, 1);
-  const mat = new MeshBasicMaterial({ color: 0xffff00 });
-  return new Mesh(geo, mat);
-}
+import { DEG2RAD } from "three/src/math/MathUtils.js";
 
 export function main(root: HTMLElement): void {
   const { width, height } = root.getBoundingClientRect();
@@ -20,24 +16,44 @@ export function main(root: HTMLElement): void {
   // Setup renderer
   const renderer = new WebGLRenderer();
   renderer.setSize(width, height);
+  root.appendChild(renderer.domElement);
 
-  // Setup placeholder scene
+  // Scene
   const scene = new Scene();
-  const camera = new PerspectiveCamera(75, width / height, 0.1, 1000);
-  const cube = makeCube();
-  scene.add(cube);
 
-  camera.position.z = 5;
+  // Camera
+  const camera = new PerspectiveCamera(75, width / height, 0.1, 1000);
+  camera.position.z = 10;
+  camera.position.x = 10;
+  camera.position.y = 15;
   camera.lookAt(new Vector3(0, 0, 0));
 
+  // Placeholder geometry
+  const geo = new BoxGeometry(1, 2, 1);
+
+  // Player placeholder
+  const mat = new MeshBasicMaterial({ color: 0x00ff00 });
+  const playerCube = new Mesh(geo, mat);
+  playerCube.position.y = 1;
+  scene.add(playerCube);
+
+  // Enemy Placeholder
+  const enemyMat = new MeshBasicMaterial({ color: 0xff0000 });
+  const enemyCube = new Mesh(geo, enemyMat);
+  enemyCube.position.y = 1;
+  enemyCube.position.z = -5;
+  scene.add(enemyCube);
+
+  // Ground placeholder
+  const groundGeo = new PlaneGeometry(50, 50, 50, 50);
+  const groundMat = new MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+  const ground = new Mesh(groundGeo, groundMat);
+  ground.rotateX(DEG2RAD * 90);
+  scene.add(ground);
+
   function tick(): void {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render(scene, camera);
   }
 
   renderer.setAnimationLoop(tick);
-
-  // Add to DOM
-  root.appendChild(renderer.domElement);
 }
