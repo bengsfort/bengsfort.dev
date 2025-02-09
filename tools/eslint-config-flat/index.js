6c03367d550abebcf20a02bb877277161e0b98ac
bengsfort <bengsfort@gmail.com>
2025-02-09 14:10:47 +0200

    Fix eslint config issues; make common package.

--- tools/eslint-config-flat/index.js
+++ tools/eslint-config-flat/index.js
@@ -1,5 +1,5 @@
 import tsEslint from 'typescript-eslint';
-
+import stylistic from '@stylistic/eslint-plugin';
 import pluginPromise from 'eslint-plugin-promise';
 import pluginPrettier from 'eslint-plugin-prettier/recommended';
 
@@ -24,127 +24,128 @@ import pluginPrettier from 'eslint-plugin-prettier/recommended';
  */
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
-export default [
-  ...tsEslint.configs.strict,
-  pluginPromise.configs['flat/recommended'],
-  {
-    rules: {
-      // General
-      // Code Cleanliness
-      curly: ['error', 'all'],
-      semi: ['error', 'always'],
-      eqeqeq: ['error', 'always'],
-      'max-len': 'off',
-      'no-console': 'warn', // Warning to encourage using scoped loggers.
-      'no-plusplus': 'off',
-      'no-unused-vars': 'off', // Disabled as it is handled by typescript-eslint
-      'no-underscore-dangle': 'off',
-      'class-methods-use-this': 'off', // Not a fan of forcing all methods to static.
-      'sort-imports': [
-        'error',
-        {
-          ignoreCase: false,
-          ignoreDeclarationSort: true,
-          ignoreMemberSort: false,
-          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-          allowSeparatedGroups: true,
-        },
-      ],
-      'padding-line-between-statements': [
-        'error',
-        { blankLine: 'always', prev: 'block-like', next: 'block-like' },
-      ],
-      // Potential bugs
-      'no-prototype-builtins': 'off',
-      'constructor-super': 'error',
-      'no-this-before-super': 'error',
-      'no-restricted-imports': [
-        'error',
-        {
-          patterns: [
-            {
-              group: ['src/*'],
-              message: 'You should import using alias or with relative path',
-            },
-          ],
-        },
-      ],
-      'no-extra-boolean-cast': [
-        'error',
-        {
-          enforceForLogicalOperands: true,
-        },
-      ],
-
-      // Typescript
-      // Code cleanliness
-      '@typescript-eslint/array-type': [
-        'error',
-        {
-          default: 'array',
-          readonly: 'array',
-        },
-      ],
-      '@typescript-eslint/adjacent-overload-signatures': 'error',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          vars: 'local',
-          args: 'after-used',
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          ignoreRestSiblings: false,
-        },
-      ],
-      // Potential bugs
-      '@typescript-eslint/no-non-null-assertion': 'error',
-      '@typescript-eslint/explicit-module-boundary-types': 'error',
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/explicit-member-accessibility': [
-        'error',
-        {
-          accessibility: 'explicit',
-          overrides: {
-            constructors: 'off',
+export default function makeConfig(configRoot, tsConfig) {
+  return [
+    ...tsEslint.configs.strict,
+    pluginPromise.configs['flat/recommended'],
+    {
+      plugins: {
+        '@stylistic': stylistic,
+      },
+      rules: {
+        // General
+        // Code Cleanliness
+        curly: ['error', 'all'],
+        '@stylistic/semi': ['error', 'always'],
+        eqeqeq: ['error', 'always'],
+        'max-len': 'off',
+        'no-console': 'warn', // Warning to encourage using scoped loggers.
+        'no-plusplus': 'off',
+        'no-unused-vars': 'off', // Disabled as it is handled by typescript-eslint
+        'no-underscore-dangle': 'off',
+        'class-methods-use-this': 'off', // Not a fan of forcing all methods to static.
+        'sort-imports': [
+          'error',
+          {
+            ignoreCase: false,
+            ignoreDeclarationSort: true,
+            ignoreMemberSort: false,
+            memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+            allowSeparatedGroups: true,
           },
-        },
-      ],
-      '@typescript-eslint/member-ordering': [
-        'error',
-        {
-          default: [
-            // Index signature
-            'signature',
+        ],
+        '@stylistic/padding-line-between-statements': [
+          'error',
+          { blankLine: 'always', prev: 'block-like', next: 'block-like' },
+        ],
+        // Potential bugs
+        'no-prototype-builtins': 'off',
+        'constructor-super': 'error',
+        'no-this-before-super': 'error',
+        'no-restricted-imports': [
+          'error',
+          {
+            patterns: [
+              {
+                group: ['src/*'],
+                message: 'You should import using alias or with relative path',
+              },
+            ],
+          },
+        ],
+        'no-extra-boolean-cast': [
+          'error',
+          {
+            enforceForLogicalOperands: true,
+          },
+        ],
 
-            // Fields
-            'public-field',
-            'protected-field',
-            'private-field',
-            'field',
-            'constructor',
-            'private-method',
-            'protected-method',
-            'public-method',
-            'method',
-          ],
-        },
-      ],
-      '@typescript-eslint/padding-line-between-statements': [
-        'error',
-        {
-          blankLine: 'always',
-          prev: '*',
-          next: ['interface'],
-        },
-        {
-          blankLine: 'always',
-          prev: ['interface'],
-          next: '*',
-        },
-      ],
+        // Typescript
+        // Code cleanliness
+        '@typescript-eslint/array-type': [
+          'error',
+          {
+            default: 'array',
+            readonly: 'array',
+          },
+        ],
+        '@typescript-eslint/adjacent-overload-signatures': 'error',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            vars: 'local',
+            args: 'after-used',
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            ignoreRestSiblings: false,
+          },
+        ],
+        // Potential bugs
+        '@typescript-eslint/no-non-null-assertion': 'error',
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/explicit-member-accessibility': [
+          'error',
+          {
+            accessibility: 'explicit',
+            overrides: {
+              constructors: 'off',
+            },
+          },
+        ],
+        '@typescript-eslint/member-ordering': [
+          'error',
+          {
+            default: [
+              // Index signature
+              'signature',
+
+              // Fields
+              'public-field',
+              'protected-field',
+              'private-field',
+              'field',
+              'constructor',
+              'private-method',
+              'protected-method',
+              'public-method',
+              'method',
+            ],
+          },
+        ],
+      },
     },
-  },
-  pluginPrettier,
-];
+    {
+      languageOptions: {
+        parserOptions: {
+          projectService: true,
+          project: tsConfig,
+          tsconfigRootDir: configRoot,
+        },
+      },
+    },
+    pluginPrettier,
+  ];
+}
